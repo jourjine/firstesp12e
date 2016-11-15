05cc67bbb7cce68d50d49d6685134ba8bd822c5c
jourjine <jourjine@gmail.com>
2016-11-15 14:17:09 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,216 @@
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+//#include <mdns.h>
 #include <ESP8266mDNS.h>
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
+//#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+#include <LedControl.h>
+//#include "brzo_i2c\brzo_i2c.h"
+#include <Wire.h>
+#include <SparkFunBME280.h>
+//#include <BH1750.h>
+BME280 mySensor;
+
+
+void bme280sensorsetup() {
+  //***Driver settings********************************//
+	//commInterface can be I2C_MODE or SPI_MODE
+	//specify chipSelectPin using arduino pin names
+	//specify I2C address.  Can be 0x77(default) or 0x76
+
+	//For I2C, enable the following and disable the SPI section
+	mySensor.settings.commInterface = I2C_MODE;
+	mySensor.settings.I2CAddress = 0x76;
+
+	//For SPI enable the following and dissable the I2C section
+	//mySensor.settings.commInterface = SPI_MODE;
+	//mySensor.settings.chipSelectPin = 10;
+
+
+	//***Operation settings*****************************//
+
+	//renMode can be:
+	//  0, Sleep mode
+	//  1 or 2, Forced mode
+	//  3, Normal mode
+	mySensor.settings.runMode = 3; //Normal mode
+
+	//tStandby can be:
+	//  0, 0.5ms
+	//  1, 62.5ms
+	//  2, 125ms
+	//  3, 250ms
+	//  4, 500ms
+	//  5, 1000ms
+	//  6, 10ms
+	//  7, 20ms
+	mySensor.settings.tStandby = 0;
+
+	//filter can be off or number of FIR coefficients to use:
+	//  0, filter off
+	//  1, coefficients = 2
+	//  2, coefficients = 4
+	//  3, coefficients = 8
+	//  4, coefficients = 16
+	mySensor.settings.filter = 0;
+
+	//tempOverSample can be:
+	//  0, skipped
+	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+	mySensor.settings.tempOverSample = 1;
+
+	//pressOverSample can be:
+	//  0, skipped
+	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+    mySensor.settings.pressOverSample = 1;
+
+	//humidOverSample can be:
+	//  0, skipped
+	//  1 through 5, oversampling *1, *2, *4, *8, *16 respectively
+	mySensor.settings.humidOverSample = 1;
+
+	//Serial.begin(57600);
+	Serial.print("Program Started\n");
+	Serial.print("Starting BME280... result of .begin(): 0x");
+
+	//Calling .begin() causes the settings to be loaded
+	delay(10);  //Make sure sensor had enough time to turn on. BME280 requires 2ms to start up.
+	Serial.println(mySensor.begin(), HEX);
+
+	Serial.print("Displaying ID, reset and ctrl regs\n");
+
+	Serial.print("ID(0xD0): 0x");
+	Serial.println(mySensor.readRegister(BME280_CHIP_ID_REG), HEX);
+	Serial.print("Reset register(0xE0): 0x");
+	Serial.println(mySensor.readRegister(BME280_RST_REG), HEX);
+	Serial.print("ctrl_meas(0xF4): 0x");
+	Serial.println(mySensor.readRegister(BME280_CTRL_MEAS_REG), HEX);
+	Serial.print("ctrl_hum(0xF2): 0x");
+	Serial.println(mySensor.readRegister(BME280_CTRL_HUMIDITY_REG), HEX);
+
+	Serial.print("\n\n");
+
+	Serial.print("Displaying all regs\n");
+	uint8_t memCounter = 0x80;
+	uint8_t tempReadData;
+	for(int rowi = 8; rowi < 16; rowi++ )
+	{
+		Serial.print("0x");
+		Serial.print(rowi, HEX);
+		Serial.print("0:");
+		for(int coli = 0; coli < 16; coli++ )
+		{
+			tempReadData = mySensor.readRegister(memCounter);
+			Serial.print((tempReadData >> 4) & 0x0F, HEX);//Print first hex nibble
+			Serial.print(tempReadData & 0x0F, HEX);//Print second hex nibble
+			Serial.print(" ");
+			memCounter++;
+		}
+		Serial.print("\n");
+	}
+
+
+	Serial.print("\n\n");
+
+	Serial.print("Displaying concatenated calibration words\n");
+	Serial.print("dig_T1, uint16: ");
+	Serial.println(mySensor.calibration.dig_T1);
+	Serial.print("dig_T2, int16: ");
+	Serial.println(mySensor.calibration.dig_T2);
+	Serial.print("dig_T3, int16: ");
+	Serial.println(mySensor.calibration.dig_T3);
+
+	Serial.print("dig_P1, uint16: ");
+	Serial.println(mySensor.calibration.dig_P1);
+	Serial.print("dig_P2, int16: ");
+	Serial.println(mySensor.calibration.dig_P2);
+	Serial.print("dig_P3, int16: ");
+	Serial.println(mySensor.calibration.dig_P3);
+	Serial.print("dig_P4, int16: ");
+	Serial.println(mySensor.calibration.dig_P4);
+	Serial.print("dig_P5, int16: ");
+	Serial.println(mySensor.calibration.dig_P5);
+	Serial.print("dig_P6, int16: ");
+	Serial.println(mySensor.calibration.dig_P6);
+	Serial.print("dig_P7, int16: ");
+	Serial.println(mySensor.calibration.dig_P7);
+	Serial.print("dig_P8, int16: ");
+	Serial.println(mySensor.calibration.dig_P8);
+	Serial.print("dig_P9, int16: ");
+	Serial.println(mySensor.calibration.dig_P9);
+
+	Serial.print("dig_H1, uint8: ");
+	Serial.println(mySensor.calibration.dig_H1);
+	Serial.print("dig_H2, int16: ");
+	Serial.println(mySensor.calibration.dig_H2);
+	Serial.print("dig_H3, uint8: ");
+	Serial.println(mySensor.calibration.dig_H3);
+	Serial.print("dig_H4, int16: ");
+	Serial.println(mySensor.calibration.dig_H4);
+	Serial.print("dig_H5, int16: ");
+	Serial.println(mySensor.calibration.dig_H5);
+	Serial.print("dig_H6, uint8: ");
+	Serial.println(mySensor.calibration.dig_H6);
+
+	Serial.println();
+}
+
+void bme280loop() {
+  //Each loop, take a reading.
+	//Start with temperature, as that data is needed for accurate compensation.
+	//Reading the temperature updates the compensators of the other functions
+	//in the background.
+
+	Serial.print("Temperature: ");
+	Serial.print(mySensor.readTempC(), 2);
+	Serial.println(" degrees C");
+
+	Serial.print("Temperature: ");
+	Serial.print(mySensor.readTempF(), 2);
+	Serial.println(" degrees F");
+
+	Serial.print("Pressure: ");
+	Serial.print(mySensor.readFloatPressure(), 2);
+	Serial.println(" Pa");
+
+	Serial.print("Altitude: ");
+	Serial.print(mySensor.readFloatAltitudeMeters(), 2);
+	Serial.println("m");
+
+	Serial.print("Altitude: ");
+	Serial.print(mySensor.readFloatAltitudeFeet(), 2);
+	Serial.println("ft");
+
+	Serial.print("%RH: ");
+	Serial.print(mySensor.readFloatHumidity(), 2);
+	Serial.println(" %");
+
+	Serial.println();
+
+	delay(1000);
+}
+
+/*
+ Now we need a LedControl to work with.
+ ***** These pin numbers will probably not work with your hardware *****
+ pin 12 is connected to the DataIn
+ pin 11 is connected to the CLK
+ pin 10 is connected to LOAD
+ We have only a single MAX72XX.
+ */
+ LedControl lc=LedControl(14,13,12,1);
+
+  unsigned long delaytime=250;
 
 
   WiFiManager wifiManager;
 
-  IPAddress server(192, 168, 1, ***); // вставить свой IP
+  IPAddress server(192, 168, 1, ****);
 
   void callback(char* topic, byte* payload, unsigned int length) {
     // handle message arrived
@@ -55,7 +256,7 @@ char *dtostrf(double val, signed char width, unsigned char prec, char *s);
 void setup() {
   Serial.begin(115200);
 
-  wifiManager.autoConnect("ESP-wifi", "wifimag");
+  wifiManager.autoConnect("ESP-wifi", "12345678");
 
   ArduinoOTA.onStart([]() {
       Serial.println("Start");
@@ -77,26 +278,133 @@ void setup() {
 
    ArduinoOTA.begin();
 
-   if (client.connect("espClient", "login", "password")) {  // Вставить свои логин и пароль для mqtt
+   if (client.connect("espClient", "*****", "*****")) {
     client.publish("outTopic","hello world");
     client.subscribe("inTopic");
    }
 
+
+
+   /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+
+bme280sensorsetup();
+
+}
+
+
+/*
+ This method will display the characters for the
+ word "Arduino" one after the other on digit 0.
+ */
+void writeArduinoOn7Segment() {
+  lc.setChar(0,0,'a',false);
+  delay(delaytime);
+  lc.setRow(0,0,0x05);
+  delay(delaytime);
+  lc.setChar(0,0,'d',false);
+  delay(delaytime);
+  lc.setRow(0,0,0x1c);
+  delay(delaytime);
+  lc.setRow(0,0,B00010000);
+  delay(delaytime);
+  lc.setRow(0,0,0x15);
+  delay(delaytime);
+  lc.setRow(0,0,0x1D);
+  delay(delaytime);
+  lc.clearDisplay(0);
+  delay(delaytime);
+}
+
+void printNumber(int v) {
+    int ones;
+    int tens;
+    int hundreds;
+
+    boolean negative=false;
+
+    if(v < -9999 || v > 9999)
+        return;
+    if(v<0) {
+        negative=true;
+        v=v*-1;
+    }
+    ones=v%10;
+    v=v/10;
+    tens=v%10;
+    v=v/10; hundreds=v;
+    if(negative) {
+        //print character '-' in the leftmost column
+        lc.setChar(0,3,'-',false);  }
+    else {
+        //print a blank in the sign column
+        lc.setChar(0,3,' ',false);
+    }
+    //Now print the number digit by digit
+    lc.setDigit(0,2,(byte)hundreds,false);
+    lc.setDigit(0,2,(byte)hundreds,false);
+    lc.setDigit(0,1,(byte)tens,false);
+    lc.setDigit(0,0,(byte)ones,false);
+}
 void vddpub() {
   vdd = system_get_vdd33()/1000.0*1.063; //*1.051;
   dtostrf(vdd, 4, 2, buffer);
   client.publish("/esp/voltage", buffer);
+  lc.setDigit(0,6,1,false);
+  lc.setDigit(0,5,4,false);
+  lc.setDigit(0,4,2,true);
+  lc.setDigit(0,3,6,false);
+  lc.setChar(0,2,'O',false);
+  lc.setChar(0,1,'C',false);
+  //lc.setChar(0,0,'O',false);
+
+//  printNumber(vdd);
 }
+
+/*
+  This method will scroll all the hexa-decimal
+ numbers and letters on the display. You will need at least
+ four 7-Segment digits. otherwise it won't really look that good.
+ */
+void scrollDigits() {
+  for(int i=0;i<13;i++) {
+    lc.setDigit(0,3,i,false);
+    lc.setDigit(0,2,i+1,false);
+    lc.setDigit(0,1,i+2,false);
+    lc.setDigit(0,0,i+3,false);
+    delay(delaytime);
+  }
+  lc.clearDisplay(0);
+  delay(delaytime);
+}
+
 
 void loop() {
 
   currentTime = millis();                           // считываем время, прошедшее с момента запуска программы
   if(currentTime >= (loopTime + 2000) ){              // сравниваем текущий таймер с переменной loopTime + 1 секунда
   vddpub();
+  // uint16_t lux = lightMeter.readLightLevel();
+  // Serial.print("Light: ");
+  // Serial.print(lux);
+  // Serial.println(" lx");
+
+
   loopTime = currentTime;                         // в loopTime записываем новое значение
   }
 
 ArduinoOTA.handle();
 client.loop();
+bme280loop();
+
+//writeArduinoOn7Segment();
+//  scrollDigits();
 
 }
